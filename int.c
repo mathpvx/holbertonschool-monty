816e6b31b731fdf6c820152ebac7022dca33430f
mathpvx <mathilde.pavaux@gmail.com>
2024-01-12 06:15:36 -0800

    function to check if its an integer

---
+++ int.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * check_int - check if input is an integer
+ * @str: the tring to check
+ * Return: 1 or -1
+ */
+int check_int(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i] && str[i] != '-')
+				return (-1);
+	}
+	return (1);
+}
--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ FILE *open_file(const char *filename);
 void execute_opcode(stack_t **stack, char *opcode, unsigned int line_num);
 void push(stack_t **stack, unsigned int line_num);
 void pall(stack_t **stack, unsigned int line_num);
+int check_int(char *str);
 
 #endif
